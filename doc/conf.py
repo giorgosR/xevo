b2333f6bf9d0e0afe4882000622b357eb4b1d43c
giorgosR <giorgosragos@gmail.com>
2020-11-15 15:14:50 +0000

    update version on conf.py [skip ci]

--- doc/conf.py
+++ doc/conf.py
@@ -31,9 +31,9 @@ copyright = u'2020, Georgios E. Ragkousis'
 author = u'Georgios E. Ragkousis'
 
 # The short X.Y version
-version = u'0.0.1.0'
+version = u'0.0.2.0'
 # The full version, including alpha/beta/rc tags
-release = u'0.0.1.0'
+release = u'0.0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
